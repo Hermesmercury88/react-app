6c3cc20176e8e4201f653a9e03369749d771af8f
Hermesmercury88 <164739504+Hermesmercury88@users.noreply.github.com>
2024-10-08 14:59:27 +0700

    first commit

---
+++ .history/react-app/src/App_20241008141626.js
@@ -0,0 +1,66 @@
+import './App.css';
+import './index.css'; 
+import React from 'react';
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
+import { Route, BrowserRouter, Routes, Outlet, Navigate, useLocation } from 'react-router-dom';
+import Dashboard from './page/Dashboard';
+import Users from './page/User';
+import AboutUs from './page/About';
+import Account from './page/Account';
+import Login from './page/Login';
+import Register from './page/Register';
+import NotPage from './page/Notpage';
+import MyBlog from './page/MyBlog';
+import AddBlog from './page/AddBlog';
+import { useAuth } from './AuthProvider';
+import { AuthProvider } from './AuthProvider';
+
+const ProtectedRoute = ({ children }) => {
+  const { user } = useAuth();
+  const location = useLocation();
+  if (!user) {
+   return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+  return children;
+ };
+
+const LayoutAdmin = () => {
+  return (
+    <ProtectedRoute>
+      <div className="flex">
+      <Sidebar />
+      <div className="flex-1">
+        <Header />
+        <div className="m-6 p-10 bg-gray-50 min-h-screen rounded-lg">
+          <Outlet /> {/* Outlet ใช้สำหรับแสดงคอมโพเน้นลูกใน LayoutAdmin */}
+        </div>
+      </div>
+    </div>
+    </ProtectedRoute>
+  );
+};
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+      <Routes>
+        <Route element={<LayoutAdmin />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/user" element={<Users />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/myblog" element={<MyBlog />} />
+          <Route
+        </Route>
+        <Route path="*" element={<NotPage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
